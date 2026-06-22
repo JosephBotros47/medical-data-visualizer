b42863346c8de7a69c9753a0dbc7003577472300
Joseph Botros <josephbotroslll@gmail.com>
2026-06-22 10:47:09 +0300

    Add files via upload

---
+++ medical_data_visualizer.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv(df = pd.read_csv('https://raw.githubusercontent.com/freeCodeCamp/boilerplate-medical-data-visualizer/main/medical_examination.csv'))
+
+df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2)).apply(lambda x: 1 if x > 25 else 0)
+
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
+
+def draw_cat_plot():
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+
+    graph = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar')
+    fig = graph.fig
+
+    fig.savefig('catplot.png')
+    return fig
+
+def draw_heat_map():
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
+
+    corr = df_heat.corr()
+
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+    fig, ax = plt.subplots(figsize=(12, 12))
+
+    sns.heatmap(
+        corr, 
+        mask=mask, 
+        annot=True, 
+        fmt='.1f', 
+        center=0, 
+        square=True, 
+        linewidths=.5, 
+        cbar_kws={'shrink': .5}, 
+        ax=ax
+    )
+
+    fig.savefig('heatmap.png')
+    return fig
